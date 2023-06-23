fb2c9bf64aea1294526042bcda12a3b9ec91bf3b
Charlie M. Abuloc <132842523+charles017@users.noreply.github.com>
2023-06-23 20:37:40 +0800

    initial upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let playersName = prompt(`What is your name?`);
+
+document.getElementById("demo").innerHTML = `Welcome ${playersName}!`;
+
+function question() {
+  const ansWer = prompt(`What is the capital of the Philippines?`);
+  if (ansWer.toUpperCase == "MANILA") {
+    document.getElementById("demo2").innerHTML = `You are correct!`;
+  } else {
+    document.getElementById("demo2").innerHTML = `You are wrong!`;
+  }
+}
+
+let text = prompt(`Please help us improve by leaving a short comment`);
+alert(`You have written ${text.length} number of characters`);
