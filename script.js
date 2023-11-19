8ea13253c5b657b287fa647b2fa4152cf53fc2cc
Charlie M. Abuloc <132842523+charles017@users.noreply.github.com>
2023-11-19 09:57:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.getElementById("demo").innerHTML = `Welcome ${playersName}!`;
 function question() {
   let ansWer = prompt(`What is the capital of the Philippines?`).toUpperCase();
   if (ansWer == "MANILA") {
-    document.getElementById("demo2").innerHTML = `You are CORRECT!`;
+    document.getElementById("demo2").innerHTML = `You are CORRECT!`
     document.body.style.backgroundColor = 'green';
   } else {
-    document.getElementById("demo2").innerHTML = `You are WRONG!`;
+    document.getElementById("demo2").innerHTML = `You are WRONG!`
     document.body.style.backgroundColor = 'red';
   }
 }
